4bd0f0b8f1db77d5bb09690d45d69d10cef8d6cb
Patrick <champion790713@gmail.com>
2023-01-06 16:04:10 +0800

    [FlowTest] add factory method to create sut

--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -8,36 +8,29 @@ import XCTest
 
 final class FlowTest: XCTestCase {
     
+    let router = RouterSpy()
+    
     func test_start_withNoQuestions_doesNotRouteToQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions: [], router: router)
-        sut.start()
+        makeSUT(questions: []).start()
         XCTAssertTrue(router.routedQuestions.isEmpty)
     }
     
     func test_start_withOneQuestion_routesToCorrectQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions: ["Q1"], router: router)
-        sut.start()
+        makeSUT(questions: ["Q1"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
     func test_start_WithOneQuestion_routesToCorrectQuestion_2() {
-        let router = RouterSpy()
-        let sut = Flow(questions: ["Q2"], router: router)
-        sut.start()
+        makeSUT(questions: ["Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q2"])
     }
     
     func test_start_withTwoQuestions_routesToFirstQuestion() {
-        let router = RouterSpy()
-        let sut = Flow(questions: ["Q1" ,"Q2"], router: router)
-        sut.start()
+        makeSUT(questions: ["Q1" ,"Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
     func test_startTwice_withTwoQuestions_routesToFirstQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1" ,"Q2"], router: router)
         sut.start()
         sut.start()
@@ -46,13 +39,17 @@ final class FlowTest: XCTestCase {
     }
     
     func test_startAndAnswerFirstQuestion_withTwoQuestions_routesToSecondQuestion() {
-        let router = RouterSpy()
         let sut = Flow(questions: ["Q1" ,"Q2"], router: router)
         sut.start()
         
         router.answerCallback("A1")
         
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
+    }
+    
+    // MARK: - Helpers
+    func makeSUT(questions: [String]) -> Flow {
+        return Flow(questions: questions, router: router)
     }
     
     class RouterSpy: Router {
